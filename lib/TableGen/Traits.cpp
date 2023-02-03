GPUOpen-Drivers/llvm-dialects

commit b82c0806ae48d2f92eadeafecdda3239a32a48f5

Represent the new memory(...) attribute in .td files
v2:
- use "read" and "write" instead of "ref" and "mod"

lib/TableGen/Traits.cpp
@@ -16,10 +16,72 @@
 
 #include "llvm-dialects/TableGen/Traits.h"
 
+#include "llvm-dialects/TableGen/Format.h"
+
 #include "llvm/TableGen/Record.h"
 
 using namespace llvm;
 using namespace llvm_dialects;
+
+namespace {
+
+class LlvmEnumAttributeTrait : public LlvmAttributeTrait {
+public:
+  LlvmEnumAttributeTrait() : LlvmAttributeTrait(Kind::LlvmEnumAttributeTrait) {}
+
+  void init(GenDialectsContext *context, llvm::Record *record) override;
+
+  void addAttribute(llvm::raw_ostream &out, FmtContext &fmt) const override;
+
+  llvm::StringRef getLlvmEnum() const { return m_llvmEnum; }
+
+  static bool classof(const Trait *t) {
+    return t->getKind() == Kind::LlvmEnumAttributeTrait;
+  }
+
+private:
+  std::string m_llvmEnum;
+};
+
+class LlvmMemoryAttributeTrait : public LlvmAttributeTrait {
+public:
+  LlvmMemoryAttributeTrait()
+      : LlvmAttributeTrait(Kind::LlvmMemoryAttributeTrait) {}
+
+  void init(GenDialectsContext *context, llvm::Record *record) override;
+
+  void addAttribute(llvm::raw_ostream &out, FmtContext &fmt) const override;
+
+  static bool classof(const Trait *t) {
+    return t->getKind() == Kind::LlvmMemoryAttributeTrait;
+  }
+
+  struct Effect {
+    bool read = false;
+    bool write = false;
+    SmallVector<std::string> locations; // empty vector means all locations
+  };
+
+private:
+  SmallVector<Effect> m_effects;
+  std::string m_llvmEnum;
+};
+
+} // anonymous namespace
+
+std::unique_ptr<Trait> Trait::fromRecord(GenDialectsContext *context,
+                                         llvm::Record *traitRec) {
+  std::unique_ptr<Trait> result;
+  if (traitRec->isSubClassOf("LlvmEnumAttributeTrait")) {
+    result = std::make_unique<LlvmEnumAttributeTrait>();
+  } else if (traitRec->isSubClassOf("Memory")) {
+    result = std::make_unique<LlvmMemoryAttributeTrait>();
+  } else {
+    report_fatal_error(Twine("unsupported trait: ") + traitRec->getName());
+  }
+  result->init(context, traitRec);
+  return result;
+}
 
 void Trait::init(GenDialectsContext *context, Record *record) {
   m_record = record;
@@ -27,7 +89,107 @@ void Trait::init(GenDialectsContext *context, Record *record) {
 
 StringRef Trait::getName() const { return m_record->getName(); }
 
-void LlvmAttributeTrait::init(GenDialectsContext *context, Record *record) {
-  Trait::init(context, record);
+void LlvmEnumAttributeTrait::init(GenDialectsContext *context, Record *record) {
+  LlvmAttributeTrait::init(context, record);
   m_llvmEnum = record->getValueAsString("llvmEnum");
+}
+
+void LlvmEnumAttributeTrait::addAttribute(raw_ostream &out,
+                                          FmtContext &fmt) const {
+  out << tgfmt("$attrBuilder.addAttribute(::llvm::Attribute::$0);\n", &fmt,
+               getLlvmEnum());
+}
+
+void LlvmMemoryAttributeTrait::init(GenDialectsContext *context,
+                                    llvm::Record *record) {
+  LlvmAttributeTrait::init(context, record);
+
+  auto *effects = record->getValueAsListInit("effects");
+  for (auto *effectInit : *effects) {
+    Effect effect;
+    auto *effectDag = cast<DagInit>(effectInit);
+    Record *op = effectDag->getOperatorAsDef(record->getLoc());
+    if (op->getName() == "read") {
+      effect.read = true;
+    } else if (op->getName() == "write") {
+      effect.write = true;
+    } else if (op->getName() == "readwrite") {
+      effect.read = true;
+      effect.write = true;
+    } else {
+      report_fatal_error(Twine("bad operator ") + op->getName() +
+                         " in memory effect");
+    }
+
+    for (auto *locationInit :
+         make_range(effectDag->arg_begin(), effectDag->arg_end())) {
+      auto *locationDef = dyn_cast<DefInit>(locationInit);
+      auto *location = locationDef ? locationDef->getDef() : nullptr;
+      if (!location || !location->isSubClassOf("LlvmMemoryLocation")) {
+        report_fatal_error(Twine("memory effect location must be an "
+                                 "LlvmMemoryLocation, but is ") +
+                           locationInit->getAsString());
+      }
+
+      effect.locations.emplace_back(location->getValueAsString("name"));
+    }
+
+    m_effects.push_back(std::move(effect));
+  }
+}
+
+namespace {
+
+struct EffectWriter {
+  EffectWriter(FmtContext &fmt, const LlvmMemoryAttributeTrait::Effect &effect)
+      : fmt(fmt), effect(effect) {}
+
+  FmtContext &fmt;
+  const LlvmMemoryAttributeTrait::Effect &effect;
+};
+
+raw_ostream &operator<<(raw_ostream &out, const EffectWriter &writer) {
+  StringRef mri;
+  if (writer.effect.read && writer.effect.write)
+    mri = "::llvm::ModRefInfo::ReadWrite";
+  else if (writer.effect.write)
+    mri = "::llvm::ModRefInfo::Mod";
+  else
+    mri = "::llvm::ModRefInfo::Ref";
+
+  if (writer.effect.locations.empty()) {
+    out << tgfmt("::llvm::MemoryEffects($0)", &writer.fmt, mri);
+  } else {
+    bool first = true;
+    for (const auto &location : writer.effect.locations) {
+      if (!first)
+        out << " | ";
+      first = false;
+      out << tgfmt("::llvm::MemoryEffects(::llvm::Location::$0, $1)",
+                   &writer.fmt, location, mri);
+    }
+  }
+
+  return out;
+}
+
+} // anonymous namespace
+
+void LlvmMemoryAttributeTrait::addAttribute(raw_ostream &out,
+                                            FmtContext &fmt) const {
+  if (m_effects.empty()) {
+    out << tgfmt("$attrBuilder.addMemoryAttr(::llvm::MemoryEffects::none());\n",
+                 &fmt);
+    return;
+  }
+  if (m_effects.size() == 1) {
+    out << tgfmt("$attrBuilder.addMemoryAttr($0);\n", &fmt,
+                 EffectWriter(fmt, m_effects[0]));
+    return;
+  }
+
+  out << "{\nauto effects = ::llvm::MemoryEffects::none();\n";
+  for (const auto &effect : m_effects)
+    out << "  effects |= " << EffectWriter(fmt, effect) << ";\n";
+  out << tgfmt("  $attrBuilder.addMemoryAttr(effects);\n}\n", &fmt);
 }

include/llvm-dialects/Dialect/Dialect.td
@@ -197,15 +197,40 @@ def AttrI64 : IntegerAttr<"uint64_t", I64>;
 
 class Trait;
 
-class LlvmAttributeTrait<string llvmEnum_> : Trait {
+class LlvmEnumAttributeTrait<string llvmEnum_> : Trait {
   string llvmEnum = llvmEnum_;
 }
 
-def NoUnwind : LlvmAttributeTrait<"NoUnwind">;
-def ReadNone : LlvmAttributeTrait<"ReadNone">;
-def ReadOnly : LlvmAttributeTrait<"ReadOnly">;
-def WillReturn : LlvmAttributeTrait<"WillReturn">;
+def NoUnwind : LlvmEnumAttributeTrait<"NoUnwind">;
+def WillReturn : LlvmEnumAttributeTrait<"WillReturn">;
 
+/// Represent the LLVM `memory(...)` attribute as the OR (or union) of memory
+/// effects. An empty effects list means the operation does not access memory
+/// at all. If this trait is missing, it means that there is no bound on the
+/// memory accesses performed by the operation.
+///
+/// Each memory effect is given as a DAG `($modref location, ...)`. If the list
+/// of memory locations is empty, it means all memory may be accessed in this
+/// way.
+///
+/// Example: `Memory<[(ref), (mod ArgMem, InaccessibleMem)]>` means the
+/// operation may read from any kind of memory and write to argument and
+/// inaccessible memory.
+class Memory<list<dag> effects_> : Trait {
+  list<dag> effects = effects_;
+}
+
+def read;
+def write;
+def readwrite;
+
+class LlvmMemoryLocation<string name_> {
+  string name = name_;
+}
+
+def ArgMem : LlvmMemoryLocation<"ArgMem">;
+def InaccessibleMem : LlvmMemoryLocation<"InaccessibleMem">;
+def Other : LlvmMemoryLocation<"Other">;
 
 // ============================================================================
 /// Operation classes

include/llvm-dialects/TableGen/Traits.h
@@ -17,23 +17,32 @@
 #pragma once
 
 #include <cstdint>
+#include <memory>
 #include <string>
 
 #include "llvm/ADT/StringRef.h"
 
 namespace llvm {
+class raw_ostream;
 class Record;
 }
 
 namespace llvm_dialects {
 
+class FmtContext;
 class GenDialectsContext;
 
 class Trait {
 public:
   enum class Kind : uint8_t {
-    LlvmAttributeTrait,
+    LlvmAttributeTrait_First,
+    LlvmEnumAttributeTrait = LlvmAttributeTrait_First,
+    LlvmMemoryAttributeTrait,
+    LlvmAttributeTrait_Last = LlvmMemoryAttributeTrait,
   };
+
+  static std::unique_ptr<Trait> fromRecord(GenDialectsContext *context,
+                                           llvm::Record *record);
 
   virtual ~Trait() = default;
 
@@ -53,18 +62,14 @@ private:
 
 class LlvmAttributeTrait : public Trait {
 public:
-  LlvmAttributeTrait() : Trait(Kind::LlvmAttributeTrait) {}
+  LlvmAttributeTrait(Kind kind) : Trait(kind) {}
 
-  void init(GenDialectsContext *context, llvm::Record *record) override;
-
-  llvm::StringRef getLlvmEnum() const { return m_llvmEnum; }
+  virtual void addAttribute(llvm::raw_ostream &out, FmtContext &fmt) const = 0;
 
   static bool classof(const Trait *t) {
-    return t->getKind() == Kind::LlvmAttributeTrait;
+    return t->getKind() >= Kind::LlvmAttributeTrait_First &&
+           t->getKind() <= Kind::LlvmAttributeTrait_Last;
   }
-
-private:
-  std::string m_llvmEnum;
 };
 
 } // namespace llvm_dialects

lib/TableGen/Dialects.cpp
@@ -33,14 +33,8 @@ Trait *GenDialectsContext::getTrait(Record *traitRec) {
         "' as a constraint, but it is not a subclass of 'Constraint'");
 
   auto &result = m_traits[traitRec];
-  if (!result) {
-    if (traitRec->isSubClassOf("LlvmAttributeTrait")) {
-      result = std::make_unique<LlvmAttributeTrait>();
-    } else {
-      report_fatal_error(Twine("unsupported trait: ") + traitRec->getName());
-    }
-    result->init(this, traitRec);
-  }
+  if (!result)
+    result = Trait::fromRecord(this, traitRec);
   return result.get();
 }
 

lib/TableGen/GenDialect.cpp
@@ -299,6 +299,7 @@ void llvm_dialects::genDialectDefs(raw_ostream& out, RecordKeeper& records) {
 #include "llvm-dialects/Dialect/OpDescription.h"
 #include "llvm-dialects/Dialect/Utils.h"
 #include "llvm/IR/InstrTypes.h"
+#include "llvm/Support/ModRef.h"
 #endif // GET_INCLUDES
 
 #ifdef GET_DIALECT_DEFS
@@ -473,18 +474,21 @@ void llvm_dialects::genDialectDefs(raw_ostream& out, RecordKeeper& records) {
     if (op.traits.empty()) {
       out << "const ::llvm::AttributeList " << attrs << ";\n";
     } else {
-      out << tgfmt("const auto $0 = ::llvm::AttributeList::get($_context, "
-                   "::llvm::AttributeList::FunctionIndex, {\n", &fmt, attrs);
+      FmtContextScope scope{fmt};
+      fmt.addSubst("attrBuilder", symbols.chooseName("attrBuilder"));
 
-      for (const Trait* trait : op.traits) {
-        if (auto* llvmAttribute = dyn_cast<LlvmAttributeTrait>(trait)) {
-          out << "::llvm::Attribute::" << llvmAttribute->getLlvmEnum() << ",\n";
+      out << tgfmt("::llvm::AttrBuilder $attrBuilder{$_context};\n", &fmt);
+      for (const Trait *trait : op.traits) {
+        if (auto *llvmAttribute = dyn_cast<LlvmAttributeTrait>(trait)) {
+          llvmAttribute->addAttribute(out, fmt);
         } else {
           llvm_unreachable("unsupported trait kind");
         }
       }
 
-      out << "});\n\n";
+      out << tgfmt("const auto $0 = ::llvm::AttributeList::get($_context, "
+                   "::llvm::AttributeList::FunctionIndex, $attrBuilder);\n",
+                   &fmt, attrs);
     }
 
     LlvmTypeBuilder typeBuilder{out, symbols, fmt};

commit df13df80199950ddbaea3784d5d01b365f0903cb

Fix some trivial mistakes in the Memory attribute support
Fixes: b82c0806 ("Represent the new memory(...) attribute in .td files")

lib/TableGen/Traits.cpp
@@ -151,7 +151,7 @@ struct EffectWriter {
 raw_ostream &operator<<(raw_ostream &out, const EffectWriter &writer) {
   StringRef mri;
   if (writer.effect.read && writer.effect.write)
-    mri = "::llvm::ModRefInfo::ReadWrite";
+    mri = "::llvm::ModRefInfo::ModRef";
   else if (writer.effect.write)
     mri = "::llvm::ModRefInfo::Mod";
   else
@@ -165,7 +165,7 @@ raw_ostream &operator<<(raw_ostream &out, const EffectWriter &writer) {
       if (!first)
         out << " | ";
       first = false;
-      out << tgfmt("::llvm::MemoryEffects(::llvm::Location::$0, $1)",
+      out << tgfmt("::llvm::MemoryEffects(::llvm::MemoryEffects::Location::$0, $1)",
                    &writer.fmt, location, mri);
     }
   }