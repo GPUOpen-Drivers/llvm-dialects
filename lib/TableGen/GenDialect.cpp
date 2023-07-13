b920b9d442e3919c24106c6c43e161120e60c572
David Stuttard <david.stuttard@amd.com>
2023-07-13 10:54:22 +0100

    Include StringExtras.h
    After LLVM patch https://reviews.llvm.org/D155018, StringExtras.h is no longer
    included transitively.

--- lib/TableGen/GenDialect.cpp
+++ lib/TableGen/GenDialect.cpp
@@ -231,6 +231,7 @@ void llvm_dialects::genDialectDefs(raw_ostream& out, RecordKeeper& records) {
 #include "llvm-dialects/Dialect/Visitor.h"
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/ADT/SmallVector.h"
+#include "llvm/ADT/StringExtras.h"
 #include "llvm/IR/InstrTypes.h"
 )";
 
