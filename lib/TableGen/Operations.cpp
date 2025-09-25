2d56785ce1ec82cc682a627fbb91f276fdfbce1c
David Stuttard <david.stuttard@amd.com>
2025-09-25 10:10:14 +0100

    Cater for llvm versions

--- lib/TableGen/Operations.cpp
+++ lib/TableGen/Operations.cpp
@@ -322,7 +322,13 @@ void OperationBase::parseValueTraits(raw_ostream &errs, RecordTy *record,
   }
 
   const ListInit *List = record->getValueAsListInit("value_traits");
-  for (const Init *I : List->getElements()) {
+  for (const Init *I : List->
+#if LLVM_MAIN_REVISION && LLVM_MAIN_REVISION < 538013
+                       getValues()
+#else
+                       getElements()
+#endif
+  ) {
     if (const DagInit *DI = dyn_cast<DagInit>(I)) {
       if (DI->getNumArgs() != 1) {
         errs << "value_traits " << *DI << " is missing argument name";
