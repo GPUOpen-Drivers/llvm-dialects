5c443e2c67ebe210141965526f4e420e5feb2ba2
Mariusz Sikora <mariusz.sikora@amd.com>
2026-07-21 11:33:59 +0200

    Use lit internal shell due to changes in LLVM 24 (#143)
    LLVM change: llvm/llvm-project#203689

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -37,7 +37,7 @@ from lit.llvm.subst import ToolSubst
 config.name = 'llvm-dialects'
 
 # testFormat: The test format to use to interpret tests.
-config.test_format = lit.formats.ShTest(not llvm_config.use_lit_shell)
+config.test_format = lit.formats.ShTest()
 
 # suffixes: A list of file extensions to treat as test files. This is overridden
 # by individual lit.local.cfg files in the test subdirectories.
